038cdfae4f582abf8198b904be36e4b6c5b02f73
Alexey <adildikov@mail.ru>
2022-04-06 14:28:19 +0300

    some refactor, start of creating db

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./App.styled";
+
+const App = () => {
+  return (
+    <S.Root>
+      <h2>asf</h2>
+    </S.Root>
+  );
+};
+
+export default App;
