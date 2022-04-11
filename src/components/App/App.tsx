65d1b7dffbbca4380395d4f826672a5d6d1d9ea8
Alexey <adildikov@mail.ru>
2022-04-11 18:27:01 +0300

    add fonts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../Header";
 
 import * as S from "./App.styled";
 
 const App = () => {
   return (
     <S.Root>
+      <Header />
       <h2>asf</h2>
     </S.Root>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./Header.styled";
+
+const Header = () => {
+  return (
+    <S.Root>
+      <h2>Header</h2>
+    </S.Root>
+  );
+};
+
+export default Header;
