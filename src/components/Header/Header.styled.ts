cba388ea7bbbd12d39ad2e0f74dd84afd727de11
Alexey <adildikov@mail.ru>
2022-04-11 19:04:10 +0300

    add colors & start work at header

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,3 +1,8 @@
+import { Colors } from "src/styles/colors";
 import styled from "styled-components";
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  width: 100%;
+  height: 108px;
+  background: ${Colors.white};
+`;
