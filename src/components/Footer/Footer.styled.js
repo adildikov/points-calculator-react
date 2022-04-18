8e3aa72710dbbc6050b38efca9dd3daed4fcefba
Alexey <adildikov@mail.ru>
2022-04-18 12:54:07 +0300

    add bg for main page

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -5,4 +5,5 @@ export const Root = styled.div`
   width: 100%;
   height: 541px;
   background: url(${bgBlack});
+  background-size: cover;
 `;
--- src/components/MainPage/MainPage.styled.js
+++ src/components/MainPage/MainPage.styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { headerHeight } from "@styles/sizes"
 import Colors from '@styles/colors';
+import bgLight from "@images/bg_image_light.png";
 
 export const Root = styled.div`
   position: relative;
   width: 100%;
   min-height: calc(100vh - ${headerHeight}px);
-  background: ${Colors.white};
+  background: url(${bgLight});
+  background-size: cover;
   color: ${Colors.dark};
 `;
