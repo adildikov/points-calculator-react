ff36c39ee41646fbbb7ccb6068d5fbe25cf5dccb
Alexey <adildikov@mail.ru>
2022-04-18 16:24:56 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import * as S from "./Footer.styled";
+import logoWhite from "@images/logo_white.svg";
 
 const Footer = () => {
   return (
     <S.Root>
-      <h2>Footer</h2>
+      <S.MainBlock>
+        <S.LogoInfoWrapper>
+          <S.Logo src={logoWhite} />
+          <S.InfoWrapper>
+            <S.Phone>+7 (4822) 34-24-52</S.Phone>
+            <S.Text>170100, г. Тверь, ул. Желябова, 33</S.Text>
+          </S.InfoWrapper>
+        </S.LogoInfoWrapper>
+        <S.LinksWrapper>
+          <S.LinksCol>
+            <S.LinksHeader>О вузе</S.LinksHeader>
+            <S.LinkItem>История</S.LinkItem>
+            <S.LinkItem>ТвГУ в рейтингах</S.LinkItem>
+            <S.LinkItem>Контакты</S.LinkItem>
+            <S.LinkItem>Сайт ТвГУ</S.LinkItem>
+          </S.LinksCol>
+          <S.LinksCol>
+            <S.LinksHeader>Образование</S.LinksHeader>
+            <S.LinkItem>Абитуриенту</S.LinkItem>
+            <S.LinkItem>Сведения об образовательной организации</S.LinkItem>
+            <S.LinkItem>Приемная коммисия</S.LinkItem>
+          </S.LinksCol>
+        </S.LinksWrapper>
+      </S.MainBlock>
+      <S.LastBlock>
+        <S.Divider />
+        <S.SubText>
+          © Тверской государственный университет, 1870 - 2022
+        </S.SubText>
+      </S.LastBlock>
     </S.Root>
   );
 };
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,9 +1,107 @@
 import styled from "styled-components";
 import bgBlack from "@images/bg_image_dark.png";
+import { media } from "@styles/media";
+import Colors from "@styles/colors"
 
 export const Root = styled.div`
   width: 100%;
-  height: 541px;
+  height: 404px;
   background: url(${bgBlack});
   background-size: cover;
+  padding: 50px 0 50px 0;
+`;
+
+export const MainBlock = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Logo = styled.img`
+  width: 199px;
+  height: 66px;
+`;
+
+export const LogoInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 201px;
+`;
+
+export const InfoWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin-top: 58px;
+  color: ${Colors.white}
+`;
+
+export const Phone = styled.div`
+  text-align: center;
+  font-family: MontserratBold;
+  font-size: 20px;
+  line-height: 24px;
+`;
+
+export const Text = styled.div`
+  font-family: MontserratLight;
+  font-size: 12px;
+  line-height: 15px;
+  margin-top: 5px;
+`;
+
+export const LinksWrapper = styled.div`
+  display: flex;
+  margin-left: 239px;
+`;
+
+export const LinksCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 76px;
+  width: 210px;
+
+  &:last-child {
+    margin-right: 0px;
+  }
+`;
+
+export const LinksHeader = styled.div`
+  width: 100%;
+  margin-bottom: 30px;
+  font-family: MontserratBold;
+  font-size: 20px;
+  line-height: 24px;
+  color: ${Colors.white};
+`;
+
+export const LinkItem = styled.div`
+  width: 100%;
+  font-family: MontserratRegular;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${Colors.white};
+  margin-bottom: 20px;
+`;
+
+export const LastBlock = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Divider = styled.div`
+  margin-top: 60px;
+  width: 57%;
+  height: 1px;
+  background: ${Colors.grey};
+`;
+
+export const SubText = styled.div`
+  margin-top: 30px;
+  font-family: MontserratRegular;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  color: ${Colors.white};
 `;
